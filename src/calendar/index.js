d83f82aaa3e42b226ac05cc120ffcf3d288a9691
Juan <gurquiza8@gmail.com>
2024-01-14 14:12:14 -0600

    First commit

---
+++ src/calendar/index.js
@@ -0,0 +1,6 @@
+
+
+export * from './pages/CalendarPage';
+export * from './components/Navbar';
+
+
---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from '../';
+
+export const CalendarPage = () => {
+    return (
+        <>
+            <Navbar />
+            <div>CalendarPage</div>
+        
+        </>
+    )
+}
