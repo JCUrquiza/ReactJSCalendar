acb164a26867f5be6375e9167106158a70c0e934
Juan <gurquiza8@gmail.com>
2024-01-20 15:56:05 -0600

    Implementing open and close the modal using redux toolkit

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -6,6 +6,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 import es from 'date-fns/locale/es';
 import Swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import { useUiStore } from '../../hooks';
 
 registerLocale('es', es)
 
@@ -25,7 +26,7 @@ Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
 
-    const [isOpen, setIsOpen] = useState(true);
+    const { isDateModalOpen, closeDateModal } = useUiStore();
     const [formSubmitted, setFormSubmitted] = useState(false);
 
     const [formValues, setFormValues] = useState({
@@ -60,8 +61,7 @@ export const CalendarModal = () => {
     }
 
     const onCloseModal = () => {
-        console.log('Cerrando Modal');
-        setIsOpen( false );
+        closeDateModal();
     }
 
     const onSubmit = ( event ) => {
@@ -91,7 +91,7 @@ export const CalendarModal = () => {
             className='modal'
             overlayClassName='modal-fondo'
             closeTimeoutMS={ 200 }
-            isOpen={ isOpen }
+            isOpen={ isDateModalOpen }
             onRequestClose={ onCloseModal }
             style={ customStyles }
         >
