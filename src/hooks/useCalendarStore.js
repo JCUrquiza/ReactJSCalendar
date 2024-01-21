f1357974c98da5e45c30a3e921e95768985cb040
Juan <gurquiza8@gmail.com>
2024-01-20 18:01:19 -0600

    Edit the active event

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onAddNewEvent, onSetActiveEvent } from '../store';
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from '../store';
 
 export const useCalendarStore = () => {
     
@@ -16,6 +16,7 @@ export const useCalendarStore = () => {
         // Todo bien
         if ( calendarEvent._id ) {
             // Actualizar
+            dispatch( onUpdateEvent( calendarEvent ) );
         } else {
             // Crear
             dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
