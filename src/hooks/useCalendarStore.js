33d72c917f3a1b1c1fa3f5a6bb12bdb7af5cf659
Juan <gurquiza8@gmail.com>
2024-01-30 12:59:41 -0600

    Saving events with complete ednpoint

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,25 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { calendarApi } from '../api';
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
 
 export const useCalendarStore = () => {
     
     const dispatch = useDispatch();
     const { events, activeEvent } = useSelector( state => state.calendar );
+    const { user } = useSelector( state => state.auth);
 
     const setActiveEvent = ( calendarEvent ) => {
         dispatch( onSetActiveEvent( calendarEvent ) );
     }
 
     const startSavingEvent = async( calendarEvent ) => {
-        // TODO: Llegar al backend
-
-        // Todo bien
+        
+        // TODO: Update event
         if ( calendarEvent._id ) {
             // Actualizar
-            dispatch( onUpdateEvent( calendarEvent ) );
+            dispatch( onUpdateEvent({ ...calendarEvent }) );
         } else {
             // Crear
-            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+
+            const { data } = await calendarApi.post('/events', calendarEvent);
+            console.log({ data });
+
+            dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }));
         }
 
     }
