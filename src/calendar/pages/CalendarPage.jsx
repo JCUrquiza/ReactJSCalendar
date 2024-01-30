df3e050a47bfae189a1675bbaac6bfc88bb94631
Juan <gurquiza8@gmail.com>
2024-01-30 14:03:37 -0600

    Delete events

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -3,9 +3,11 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { CalendarEvent, CalendarModal, FabAddNew, FabDelete, Navbar } from '../';
 import { localizer, getMessagesES } from '../../helpers';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 export const CalendarPage = () => {
+
+    const { user } = useAuthStore();
 
     const { openDateModal } = useUiStore();
     const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
@@ -13,13 +15,16 @@ export const CalendarPage = () => {
     const [ lastView, setLastView ] = useState( localStorage.getItem('lastView') || 'month' );
 
     const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+        const isMyEvent = ( user.uid === event.user._id ) || ( user.uid === event.user.uid );
+
         const style = {
-            backgroudColor: '#347CF7',
+            backgroudColor: isMyEvent ? '#347CF7' : '#465660',
             borderRadius: '0px',
             opacity: 0.8,
             color: 'white'
         }
-        return {
+        return {  
             style
         }
     }
