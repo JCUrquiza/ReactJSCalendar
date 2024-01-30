2a0522320827abaa58b38308d5e47b157ba17d35
Juan <gurquiza8@gmail.com>
2024-01-30 13:46:45 -0600

    Edit events

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { calendarApi } from '../api';
 import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from '../store';
 import { convertEventsToDateEvents } from '../helpers';
+import Swal from 'sweetalert2';
 
 export const useCalendarStore = () => {
     
@@ -14,18 +15,19 @@ export const useCalendarStore = () => {
     }
 
     const startSavingEvent = async( calendarEvent ) => {
-        
-        // TODO: Update event
-        if ( calendarEvent._id ) {
-            // Actualizar
-            dispatch( onUpdateEvent({ ...calendarEvent }) );
-        } else {
-            // Crear
 
-            const { data } = await calendarApi.post('/events', calendarEvent);
-            console.log({ data });
-
+        try {
+            // TODO: Update event
+            if ( calendarEvent.id ) {
+                await calendarApi.put(`/events/${ calendarEvent.id }`, calendarEvent);
+                dispatch( onUpdateEvent({ ...calendarEvent, user }) );
+            }            
+            // Crear    
+            const { data } = await calendarApi.post('/events', calendarEvent);    
             dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }));
+        } catch (error) {
+            console.log(error);
+            Swal.fire('Error al guardar', error.response.data?.msg, 'error');
         }
 
     }
