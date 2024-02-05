a656578232b28b12408afa2f978868fb6e9fa610
Juan <gurquiza8@gmail.com>
2024-02-05 14:04:18 -0600

    Finish to testing FabDeleting component

--- src/calendar/components/FabDelete.jsx
+++ src/calendar/components/FabDelete.jsx
@@ -10,13 +10,14 @@ export const FabDelete = () => {
 
     return (
         <button 
-            className="btn btn-danger fab-danger"
+            className='btn btn-danger fab-danger'
+            aria-label='btn-delete'
             onClick={ handleDelete }
             style={{
                 display: hasEventSelected ? '' : 'none'
             }}
         >
-            <i className="fas fa-trash-alt"></i>
+            <i className='fas fa-trash-alt'></i>
         </button>
     )
 }
